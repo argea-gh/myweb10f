30d6d3b11d7e8e7a060135a4463f96368fd63814
argea-gh <argo.armail@gmail.com>
2026-01-01 14:08:20 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,15 +115,15 @@ function openProductModal(product) {
       </div>
     </div>
     
-    <div style="text-align:center; margin-top:30px;">
-      <button class="btn btn-primary" id="addToCartBtn" data-id="${product.id}">
-        ➕ Tambah ke Keranjang
-      </button>
-      <button class="btn btn-whatsapp" style="margin-left:12px;" 
-              onclick="window.open('https://wa.me/6282241900467?text=Saya%20mau%20pesan%20${encodeURIComponent(product.name)}%2C%20harga%20${priceFormatted}.', '_blank')">
-        📲 Langsung ke WhatsApp
-      </button>
-    </div>
+    <div class="btn-group">
+  <button class="btn btn-primary" id="addToCartBtn" data-id="${product.id}">
+    ➕ Tambah ke Keranjang
+  </button>
+  <a href="https://wa.me/6282241900467?text=Saya%20mau%20pesan%20${encodeURIComponent(product.name)}%2C%20harga%20${priceFormatted}." 
+     target="_blank" class="btn btn-whatsapp">
+    📲 Langsung ke WhatsApp
+  </a>
+</div>
   `;
 
   productModal.style.display = 'block';
